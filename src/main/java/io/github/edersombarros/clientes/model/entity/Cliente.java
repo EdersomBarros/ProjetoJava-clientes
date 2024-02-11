a8ae563042e3d3b07f3be343be78ad93749eb62c
Edersom Barros <edersombarros@hotmail.com>
2024-02-11 17:22:41 -0400

    validation on the client

--- src/main/java/io/github/edersombarros/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/edersombarros/clientes/model/entity/Cliente.java
@@ -5,8 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 @Entity
 @Data
@@ -17,11 +20,17 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, length = 150 )
+    @NotEmpty
     private String nome;
+
     @Column(nullable = false, length = 11)
+    @NotNull
+    @CPF
     private String cpf;
-    @Column(name = "data_cadastro")
+
+    @Column(name = "data_cadastro", updatable = false)
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
 
--- src/main/java/io/github/edersombarros/clientes/rest/ClienteController.java
+++ src/main/java/io/github/edersombarros/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import io.github.edersombarros.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -22,6 +23,35 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente) {
         return repository.save(cliente);
+    }
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Long id){
+        return repository.findById(id).orElseThrow(
+                () ->new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id){
+        repository.findById(id)
+                .map( cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow(
+                () ->new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Long id, @RequestBody Cliente clienteAtualizado){
+        repository.findById(id)
+                .map( cliente -> {
+                   cliente.setNome(clienteAtualizado.getNome());
+                   cliente.setCpf(clienteAtualizado.getCpf());
+                    return repository.save(clienteAtualizado);
+                })
+                .orElseThrow(
+                        () ->new ResponseStatusException(HttpStatus.NOT_FOUND));
+
     }
 
 }
